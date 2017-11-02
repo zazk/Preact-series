c38fe1569d598162a1b008fd579c4ac268eef7b8
zazk <juandedioz@gmail.com>
2017-11-02 00:57:09 -0500

    Added Axios

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # Preact-series
 Multifunctional Application Preact
+* Use axios for request
+* Use const for simple components
 ## Run
 ```
 # cd cli
 # yarn dev
 ```
+## Deploy in now.sh
+```
+# now
+
--- cli/src/routes/task/index.js
+++ cli/src/routes/task/index.js
@@ -1,7 +1,37 @@
-const Task = () => (
+import { h, Component } from 'preact';
+import axios from 'axios'
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+export default class Task extends Component{ 
+
+    state = {data :[]}
+
+    componentDidMount(){
+        this.getPostData().then((data) => {
+          this.setState({ data })
+          console.log("Data Loaded", data,this);
+        })
+    }
+
+    getPostData() {
+      const url = `${BASE_URL}/posts`;
+      return axios.get(url).then(response => response.data);
+    }
+    render ({},{data}){
+        return (
             <div>
-                Welcome Task Component
+                <h3>Welcome Task Component</h3>
+                {
+                    data.map((item,index)=>(
+                            <div>
+                                <strong># {index}</strong> - 
+                                {item.title}
+                            </div>
+                        ))
+                }
             </div>
         )
-
-export default Task
+    }
+}
+ 
